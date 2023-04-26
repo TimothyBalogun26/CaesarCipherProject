70a7e9146e273f083bd601dd33435fc4ce7ffb93
Timyn <tbalogun@ggc.edu>
2023-04-25 22:17:04 -0400

    Caesar Cipher class

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class CaesarCipher {
-}
+public class CaesarCipher {
+        private int shifts;
+
+        public CaesarCipher(int shifts) {
+            this.shifts = shifts;
+        }
+
+        public String encrypt(String message) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < message.length(); i++) {
+                char c = message.charAt(i);
+                if (Character.isLetter(c)) {
+                    char base = Character.isLowerCase(c) ? 'a' : 'A';
+                    c = (char) (base + (c - base + shifts) % 26);
+                }
+                result.append(c);
+            }
+            return result.toString();
+        }
+
+        public String decrypt(String message) {
+            return encrypt(message, 26 - shifts);
+        }
+
+        private String encrypt(String message, int shifts) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < message.length(); i++) {
+                char c = message.charAt(i);
+                if (Character.isLetter(c)) {
+                    char base = Character.isLowerCase(c) ? 'a' : 'A';
+                    c = (char) (base + (c - base + shifts) % 26);
+                }
+                result.append(c);
+            }
+            return result.toString();
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a message to be encrypted or decrypted: ");
+        String message = scanner.nextLine();
+        System.out.print("Enter an integer key value for encryption or decryption: ");
+        int key = scanner.nextInt();
+        scanner.nextLine(); // consume the newline character
+        System.out.print("Enter 'E' to encrypt or 'D' to decrypt: ");
+        String choice = scanner.nextLine();
+        CaesarCipher cipher = new CaesarCipher(key);
+        String result = "";
+        if (choice.equalsIgnoreCase("E")) {
+            result = cipher.encrypt(message);
+            System.out.println("Encrypted message: " + result);
+        } else if (choice.equalsIgnoreCase("D")) {
+            result = cipher.decrypt(message);
+            System.out.println("Decrypted message: " + result);
+        } else {
+            System.out.println("Invalid choice");
+        }
     }
 }
